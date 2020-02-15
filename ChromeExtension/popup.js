b3d945b9b6336df475cc86ce2ecb9386dd95bee3
Lewington <lewington@student.unimelb.edu.au>
2020-02-15 23:54:47 +1100

    use chrome session tokans

--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -1,23 +1,46 @@
-window.onload = function() {
-    chrome.storage.local.get(/* String or Array */["isLoggedIn"], function(items){
-        if (items["isLoggedIn"] == "true") {
-            document.getElementById("login").style.display = "none";
-            document.getElementById("logout").style.display = "block";
-            document.getElementById("recommendations").disabled = false;
-        } else {
-            setToLogOutState();
-        }
+var endpoint = "http://localhost:8091"
 
-    });
+window.onload = function() {
+    setupButtons();
+
     document.getElementById("body").addEventListener('click', function(event) {
+        event.preventDefault();
 
         if (event.target.id == "login") {
-            document.getElementById("button-holder").style.display = "none";
-            document.getElementById("login-holder").style.display = "block";
+            setToLoginMode()
         } else if (event.target.id == "register") {
-            alert("registering");
+            chrome.tabs.create({url: endpoint + "/html/register.html"});
         } else if (event.target.id == "submit") {
-            chrome.storage.local.set({"isLoggedIn": "true" }, function(){});
+
+            username = document.getElementById("username").value;
+            password = document.getElementById("password").value;
+            console.log(username);
+            var r = new XMLHttpRequest();
+            r.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        console.log(this.response);
+
+                        chrome.storage.local.set({"isLoggedIn": "true" }, function(){});
+                        chrome.storage.local.set({"erosai_access_token": this.response}, function(){});
+                        setToButtonMode();
+
+                    } else if (this.status == 400) {
+                        document.location.reload(true)
+                        alert("Username or Password was not recognized")
+                    } else {
+                        alert("OOPS! Something went hideously wrong, contact an admin.")
+                    }
+                }
+            };
+            
+            r.open("POST", endpoint + "/login-attempt", true);
+            r.setRequestHeader('Content-Type', 'application/json');
+            r.send(JSON.stringify({
+                Username: username,
+                Password: password,
+            }));
+            
         } else if (event.target.id == "logout") {
             chrome.storage.local.set({"isLoggedIn": "false" }, function(){});
             setToLogOutState();
@@ -27,9 +50,33 @@ window.onload = function() {
     });
 }
 
+function setupButtons() {
+    chrome.storage.local.get(/* String or Array */["isLoggedIn"], function(items){
+        if (items["isLoggedIn"] == "true") {
+            document.getElementById("login").style.display = "none";
+            document.getElementById("status").innerHTML = "You are Logged In";
+            document.getElementById("logout").style.display = "block";
+            document.getElementById("recommendations").disabled = false;
+        } else {
+            setToLogOutState();
+        }
+    });
+}
+
+function setToButtonMode() {
+    document.getElementById("button-holder").style.display = "block";
+    document.getElementById("login-holder").style.display = "none";
+    setupButtons();
+}
+
+function setToLoginMode() {
+    document.getElementById("button-holder").style.display = "none";
+    document.getElementById("login-holder").style.display = "block";
+}
 function setToLogOutState() {
     document.getElementById("logout").style.display = "none";
     document.getElementById("login").style.display = "block";
+    document.getElementById("status").innerHTML = "You are Logged Out";
     document.getElementById("recommendations").disabled = true;
 }
 
